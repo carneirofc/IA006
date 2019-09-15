51958d6cbffe49117e7deec4ecd4ecb9730ac6be
carneirofc <claudiofcarneiro@hotmail.com>
2019-09-15 18:44:42 -0300

    Working algorithm. @todo: k-fold

--- EFC1/ex01_comp.py
+++ EFC1/ex01_comp.py
@@ -5,47 +5,57 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas
 import datetime
+import numpy as np
+from statistics import mean
 
 def config_plt():
     plt.style.use('seaborn-whitegrid')
     matplotlib.use('TkAgg')
 
+def getTrainData(data, k, folds):
+    pass
 
-def getModel(y, k):
-    '''
-    :param y: Temperature array
-    :param k: delay quantity
-    :returns I and Y matrix
-    '''
-    for i in range(len(y)-1, 0, -1):
-        if i - k < 0:
-            break
-        res = '{}) y={}'.format(i, y[i])
-        res += ' x0=1'
-        for x in range(1, k+1):
-            res += ' x{}={}'.format(x, y[i-x])
-        print(res)
+def getInpRes(data, k):
+    inp = np.zeros((len(data)-k, k+1))
+    res = np.zeros((len(data)-k, 1))
+
+    for i in range(len(data)-1, k-1, -1):
+        rw = i - k
+        inp[rw][0] = 1
+        res[rw][0] = data[i]
+        for j in range(1, k+1):
+            inp[rw][j] = data[i -j]
+    return inp, res
+
+def getWeight(fi, y):
+    # p1 = (fi*fiT)-1
+    # p2 = fiT*y
+    fiT = fi.transpose()
+    p1 = np.linalg.inv(fiT.dot(fi))
+    p2 = fiT.dot(y)
+    w = p1.dot(p2)
+    return w
+
+
+def evalModel(w, test_inp, test_resp):
+    y_est = test_inp.dot(w)
+    rm = (test_resp - y_est)**2
+    print('{}\t{}\t{}\t{}'.format(len(w)-1, np.sum(rm)/len(rm), np.amin(rm), np.amax(rm)))
+    return rm
 
 if __name__ == '__main__':
-    config_plt()
-    fig = plt.figure()
-    ax = plt.axes()
-
     df_data = utils.read_data(fname='daily-minimum-temperatures.csv')
     df_data['Date'] = pandas.to_datetime(df_data['Date'])
-    temp = df_data['Temp'].values
-    date = df_data['Date'].values
-
-    #ax.plot(date, temp)
-    #plt.draw()
-    #plt.show()
 
     # Split dataset ...
     # >= 1990-01-01 Test
-    date_max= datetime.datetime(1990,1,1)
+    date_max = datetime.datetime(1990,1,1)
     df_test = df_data.loc[df_data.Date >= date_max]
     df_train = df_data.loc[df_data.Date < date_max]
-    print('Test DF', df_test.head())
-    print('Train DF', df_train.head())
 
-    getModel(df_train['Temp'].values, 2)
+    for k in range(1,31):
+        train_inp, train_resp = getInpRes(df_train['Temp'].values, k)
+        test_inp, test_resp = getInpRes(df_test['Temp'].values, k)
+
+        w = getWeight(train_inp, train_resp)
+        evalModel(w, test_inp, test_resp)
