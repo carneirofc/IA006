de454a42d224cd4c4a7fd8f7f9c5b8eba132d035
carneirofc <claudiofcarneiro@hotmail.com>
2019-09-17 08:25:53 -0300

    Update

---
+++ EFC1/ex02.py
@@ -0,0 +1,100 @@
+#!../venv/bin/python
+import tkinter
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas
+import datetime
+import numpy as np
+from statistics import mean
+from utils import read_data, config_plt, shuffle, get_tanh_inp_res as get_inp_res, norm_data
+
+
+def get_best_fold(folds, percent, train_inp, train_out):
+    '''
+    return weights, details, folds_rms.index(min(folds_rms))
+    '''
+    # print('Folding ...')
+    folds_detail = []
+    folds_rms = []
+    folds_w = []
+    split = int(len(train_inp)*percent)
+    for fold in range(folds):
+        s_train_inp, s_train_out = shuffle(train_inp, train_out)     # shuffle the bois
+        w = get_weight(s_train_inp[0:split], s_train_out[0:split])     # train
+        rms = eval_model(w, s_train_inp[split:], s_train_out[split:])  # test
+        avg = np.mean(rms)
+        folds_rms.append(avg)
+        folds_detail.append({'var':np.var(rms),'avg':avg, 'min':np.amin(rms), 'max':np.amax(rms)})
+        folds_w.append(w)
+    idx = folds_rms.index(min(folds_rms))
+    #print('Finish folding ({} folds)... Best fold:idx {}\t{}'.format(folds, idx, folds_detail[idx]))
+    return folds_w, folds_detail, idx
+
+def get_weight(fi, y):
+    # p1 = (fi*fiT)-1
+    # p2 = fiT*y
+    fiT = fi.transpose()
+    p1 = np.linalg.inv(fiT.dot(fi))
+    p2 = fiT.dot(y)
+    w = p1.dot(p2)
+    return w
+
+def eval_model(w, test_inp, test_out):
+    y_est = test_inp.dot(w)
+    rm = (test_out - y_est)**2
+    return rm
+
+if __name__ == '__main__':
+    df_data = read_data(fname='daily-minimum-temperatures.csv')
+    df_data['Date'] = pandas.to_datetime(df_data['Date'])
+    temp = df_data['Temp'].values
+    print('Dataset {} {}'.format(mean(temp), np.var(temp)))
+    #temp, m, std = norm_data(temp)
+    #df_data['Temp'] = temp
+
+    # Split dataset ...
+    # >= 1990-01-01 Test
+    # 1981 -> 1988
+    date_max = datetime.datetime(1990,1,1)
+    df_test = df_data.loc[df_data.Date >= date_max]
+    df_train = df_data.loc[df_data.Date < date_max]
+
+    folds = 10
+    percent = 0.8
+
+    estimators = []
+    with open('01info.txt', 'w+') as f:
+        f.write('Folds = {}\n)')
+        f.write('Test+Validation = {}%\n'.format(folds, percent*100))
+
+    with open('01delays.csv', 'w+') as f:
+        f.write('"k","avg","var","min","max"\n')
+        for k in range(1,31):
+            print('K {} ...'.format(k))
+            train_inp, train_out = get_inp_res(df_train['Temp'].values, k)
+            test_inp, test_out = get_inp_res(df_test['Temp'].values, k)
+
+            # Fold and return the best weight (W)
+            w_optimum, folds_detail, idx = get_best_fold(folds, percent, train_inp, train_out)
+            w_optimum, folds_detail = w_optimum[idx], folds_detail[idx]
+
+            # Test the model using W
+            rm = eval_model(w_optimum, test_inp, test_out)
+            det = {'k':len(w_optimum)-1,'avg':np.mean(rm), 'var':np.var(rm), 'min':np.amin(rm), 'max':np.max(rm), 'w':w_optimum, 'fold_det':folds_detail}
+            estimators.append(det)
+            f.write('{},{},{},{},{}\n'.format(det['k'], det['avg'], det['var'], det['min'], det['max']))
+
+    # Locate the min avg(RMS) from K delays
+    da_best = min(estimators, key=lambda x:x['avg'])
+    print(da_best['k'], da_best['avg'], da_best['var'])
+    with open('01estimator.txt', 'w+') as f:
+        f.write('W =\n {}\n'.format(da_best['w']))
+    with open('01estimator.csv', 'w+') as f:
+        f.write('"Real","Est","Error"\n')
+        test_inp, test_out = get_inp_res(df_test['Temp'].values, da_best['k'])
+        y_est = test_inp.dot(da_best['w'])
+        e = (test_out - y_est)
+        for i in range(len(e)):
+            f.write('{},{},{}\n'.format(test_out[i][0], y_est[i][0], e[i][0]))
+
+
--- EFC1/utils.py
+++ EFC1/utils.py
@@ -1,6 +1,61 @@
+import numpy as np
 import pandas
-
+import math
 def read_data(fname):
     return pandas.read_csv(fname)
 
 
+def config_plt():
+    plt.style.use('seaborn-whitegrid')
+    matplotlib.use('TkAgg')
+
+
+def shuffle(train_inp, train_out):
+    indices = np.arange(train_inp.shape[0])
+    np.random.shuffle(indices)
+    return train_inp[indices], train_out[indices]
+
+
+def norm_data(data):
+    '''
+    Z-score norm
+    :param data: np.array
+    :return :Tuple (norm np.array, mean, std)
+    '''
+    mean = data.mean()
+    std = data.std()
+    return (data - mean)/std, mean, std
+
+def get_tanh_inp_res(data, k):
+    """
+    Apply tanh(w*inp), w random generated weights following a linear dist
+    :param data:
+    :param k:
+    :return inp, res, w:
+    """
+    inp = np.zeros((len(data)-k, k+1))
+    res = np.zeros((len(data)-k, 1))
+
+    for i in range(len(data)-1, k-1, -1):
+        rw = i - k
+        inp[rw][0] = 1
+        res[rw][0] = data[i]
+        for j in range(1, k+1):
+            inp[rw][j] = data[i -j]
+    w = np.ones(len(res[0]))
+    #@todo: Randon gen w
+    inp = math.tanh(w*inp)
+    return inp, res, w
+
+
+def get_inp_res(data, k):
+    inp = np.zeros((len(data)-k, k+1))
+    res = np.zeros((len(data)-k, 1))
+
+    for i in range(len(data)-1, k-1, -1):
+        rw = i - k
+        inp[rw][0] = 1
+        res[rw][0] = data[i]
+        for j in range(1, k+1):
+            inp[rw][j] = data[i -j]
+    return inp, res
